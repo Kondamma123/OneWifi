be34b0e171e0ed39ba283c5036437847f7e8cf1a
Kondamma123 <kudumula.kondamma@capgemini.com>
2026-07-27 12:35:10 +0000

    middle_priority1

--- source/webconfig/wifi_decoder.c
+++ source/webconfig/wifi_decoder.c
@@ -2930,6 +2930,7 @@ void decode_acs_keep_out_json(const char *json_string, unsigned int num_of_radio
                     wifi_util_error_print(WIFI_CTRL,
                         "%s:%d decode_bandwidth_from_json returned error\n", __FUNCTION__,
                         __LINE__);
+                    cJSON_Delete(json);
                     return;
                 }
             } else {
